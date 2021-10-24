4e636d16a3fceea7f515b4fb3fc120fbf41172d0
Raquel Silloca <rsilloca@unsa.edu.pe>
2021-10-24 09:29:12 -0500

    Botón hacia otro activity

--- app/src/main/java/com/epis/lab05_idnp/MainActivity.kt
+++ app/src/main/java/com/epis/lab05_idnp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.epis.lab05_idnp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnExercise2 = findViewById<Button>(R.id.go_exercise_2)
+        btnExercise2.setOnClickListener {
+            val intent = Intent(this, BarGraphicActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
